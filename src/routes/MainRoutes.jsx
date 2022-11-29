e23458b283961a41fa44df93d4a955dd1ebb5917
brainsteeldrywork <brainsteel.drywork@gmail.com>
2022-11-29 17:37:07 -0300

    change basename

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -6,7 +6,7 @@ import About from '../components/About';
 
 export const MainRoutes = () => {
 return (
-    <BrowserRouter basename='/home'>
+    <BrowserRouter>
         <Routes>
             <Route path='/' element={<FirstPage />} />
             <Route exact path='/sobre-nos' element={<About />} />
